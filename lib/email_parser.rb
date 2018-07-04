ad2a896a95205ccd57e927a58960cd6debdf36f8
Vishal Sharma <vsh1819@gmail.com>
2018-07-04 10:43:12 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,7 @@ class EmailParser
   end
   
   def parse
-    email_array = @emails.split("/[,]/")
+    email_array = @emails.split("/[,\s+]/")
     result = email_array.map do |email|
       email.strip()
     end
